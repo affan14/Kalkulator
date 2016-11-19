0c2c0ad0a0f1dc04823015239f478a8e53bd4138
Affan Mohammad <affanmohammad@192.168.2.14>
2016-11-19 09:41:33 +0700

    Initial commit

---
+++ app/src/main/java/com/calc/affanmohammad/kalkulator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.calc.affanmohammad.kalkulator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // binding dari xml ke java
+    }
+}
